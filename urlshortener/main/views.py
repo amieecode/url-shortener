426538eed291e306ea55fd98b71b6063cc72a609
amieecode <enwelumchiamaka@gmail.com>
2024-08-20 18:30:40 +0100

    url shortening api intergrated

--- urlshortener/main/views.py
+++ urlshortener/main/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import pyshorteners
 
 # Create your views here.
+def shorten(request, url):
+    shortener = pyshorteners.Shortener()
+    shortened_url = shortener.tinyurl.short(url)
+    return HttpResponse(f'Shortened URL: <a href="shortened_url">{shortened_url}</a>')
