d77cd91d27a52d51e532505a11e24d7b2f2a1c65
amieecode <enwelumchiamaka@gmail.com>
2024-08-20 18:41:02 +0100

    created a form

--- urlshortener/main/views.py
+++ urlshortener/main/views.py
@@ -7,3 +7,9 @@ def shorten(request, url):
     shortener = pyshorteners.Shortener()
     shortened_url = shortener.tinyurl.short(url)
     return HttpResponse(f'Shortened URL: <a href="shortened_url">{shortened_url}</a>')
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def shorten_post(request):
+    return shorten(request, request.POST['url'])
