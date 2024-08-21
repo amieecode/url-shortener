6ecc4a7e640a03d56d77df401b23201b411662bd
amieecode <enwelumchiamaka@gmail.com>
2024-08-21 12:05:14 +0100

    Created a databse model

--- urlshortener/main/models.py
+++ urlshortener/main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    original_url = models.CharField(max_length=256)
+    url_hash = models.CharField(max_length=10)
+    creation_date = models.DateTimeField('creation_date')
