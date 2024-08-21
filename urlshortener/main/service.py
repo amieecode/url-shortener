e8e0f228a3ccbb9f11388cd829d94cfa6ebc1ca1
amieecode <enwelumchiamaka@gmail.com>
2024-08-21 13:09:56 +0100

    implemented url shortening and remove the pyshortener api

---
+++ urlshortener/main/service.py
@@ -0,0 +1,13 @@
+import random 
+import string 
+from django.utils import timezone
+from .models import LinkMapping
+
+def shorten(url):
+    random_hash = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(7))
+    mapping = LinkMapping(original_url=url, hashing=random_rash, creation_date=timezone.now())
+    mapping.save()
+    return random_hash
+
+def load_url(url_hash):
+    return LinkMapping.objects.get(hashing=url_hash)
--- urlshortener/main/views.py
+++ urlshortener/main/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-import pyshorteners
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .import service
 
 # Create your views here.
 def shorten(request, url):
-    shortener = pyshorteners.Shortener()
-    shortened_url = shortener.tinyurl.short(url)
+    shortened_url_hash = service.shorten_url
+    shortened_url = request.build_absolute_uri(reverse('redirect', args=[shortened_url_hash]))
     return HttpResponse(f'Shortened URL: <a href="shortened_url">{shortened_url}</a>')
 
 def index(request):
@@ -13,3 +14,7 @@ def index(request):
 
 def shorten_post(request):
     return shorten(request, request.POST['url'])
+
+def redirect_hash(request, url):
+    original_url = service.load_url(url_hash).original_url
+    return redirect(original_url)
